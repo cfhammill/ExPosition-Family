7a3fae81a2e2d158cdde84432a3afd2563237c07
cfhammill <cfhammill@gmail.com>
2017-08-20 10:15:30 -0400

    Add new svd tests and benchmarks
    Also updates the test bed to use an installed SlimPosition

--- Code/R/Development/SlimPosition/gsvd_test_conditions.R
+++ Code/R/Development/SlimPosition/gsvd_test_conditions.R
@@ -1,10 +1,11 @@
 ## test for canonical correlation analysies (CCA) and correspondence analysis (CA): the two most important techniques for the GSVD test.
 
-source('./R/gsvd.R')
-source('./R/tolerance.svd.R')
-source('./R/power.rebuild_matrix.R')
-source('./R/invert.rebuild_matrix.R')
-source('./R/isDiagonal.matrix.R')
+library(SlimPosition)
+#source('./R/gsvd.R')
+#source('./R/tolerance.svd.R')
+#source('./R/power.rebuild_matrix.R')
+#source('./R/invert.rebuild_matrix.R')
+#source('./R/isDiagonal.matrix.R')
 
   #authors data for CA
 load('./data/authors.rda')
@@ -15,7 +16,15 @@ sum.data <- sum(authors$ca$data)
 wi <- rowSums(authors$ca$data)/sum.data
 wj <- colSums(authors$ca$data)/sum.data
 
-ca.res <- gsvd( (authors$ca$data/sum.data) - (wi %o% wj), 1/wi, 1/wj)
+dat <- (authors$ca$data/sum.data) - (wi %o% wj)
+
+r_res <- gsvd(dat, 1/wi, 1/wj)
+cpp_res <- SlimPosition:::gsvd_new(dat, 1/wi, 1/wj)
+
+all.equal(r_res$tau, cpp_res$tau)
+
+microbenchmark::microbenchmark(gsvd(dat, 1/wi, 1/wj)
+                             , SlimPosition:::gsvd_new(dat, 1/wi, 1/wj))
 
   #wine data for CCA
 load('./data/two.table.wine.rda')
@@ -23,9 +32,22 @@ load('./data/two.table.wine.rda')
 X <- scale(wine$objective, center = T, scale = T)
 Y <- scale(wine$subjective, center = T, scale = T)
 
-cca.res <- gsvd(t(X) %*% Y, crossprod(X), crossprod(Y))
+#cca.res <- gsvd(t(X) %*% Y, crossprod(X), crossprod(Y))
 
+dat <- t(X) %*% Y
+XtX <- crossprod(X)
+YtY <- crossprod(Y)
 
+r_res <- gsvd(dat, XtX, YtY)
+cpp_res <- SlimPosition:::gsvd_eig(dat, XtX, YtY, .Machine$double.eps
+                                 , 5, 0)
+
+all.equal(r_res$d, cpp_res$d)
+
+microbenchmark::microbenchmark(gsvd(dat, XtX, YtY)
+                             , SlimPosition:::gsvd_eig(dat, XtX, YtY, .Machine$double.eps
+                                                     , 5, 0)
+                               , SlimPosition:::gsvd_new(dat, XtX, YtY, .Machine$double.eps))
 
 
 ### The most important results that need to match with any other implementation of the SVD are:
